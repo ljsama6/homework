80282ac07c83ff2f2ee9d6c525104f286c7d7825
ljsama <2817982520@qq.com>
2024-09-16 17:14:22 +0800

    冒泡排序数组代码回顾

--- Homework12.java
+++ Homework12.java
@@ -3,6 +3,13 @@ package homework;
 public class Homework12 {
     public static void main(String[] args){
         int[] arr = {20, -1, 89, 2, 890, 7};
+        //排序前
+        System.out.println("冒泡排序前的数组情况:");
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println( );
+        //冒牌排序
         for (int i = 0; i < arr.length - 1; i++){
             for (int j = 0; j < arr.length - i - 1; j++){
                 if (arr[j] > arr[j + 1]){
@@ -12,7 +19,8 @@ public class Homework12 {
                 }
             }
         }
-        System.out.println("冒泡排序数组情况:");
+        //排序后
+        System.out.println("冒泡排序后的数组情况:");
         for (int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
