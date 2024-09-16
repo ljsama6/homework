8ec0acd4060087f661878799457a522af341d743
ljsama <2817982520@qq.com>
2024-09-16 21:04:11 +0800

    冒泡排序数组代码回顾

--- Homework10.java
+++ Homework10.java
@@ -2,6 +2,7 @@ package homework;
 
 public class Homework10 {
     public static void main(String[] args) {
+        //采用建立数组，添加数值，进行冒泡排序
         int[] arr = {10, 12, 45, 90};
         int[] arrNew = new int[arr.length + 1];
         for (int i = 0; i < arr.length; i++){
