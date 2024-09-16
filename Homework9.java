1ce7fed532eadc7d7d443d705f76f211dcad4bc7
ljsama <2817982520@qq.com>
2024-09-16 16:24:52 +0800

    second commit

--- Homework9.java
+++ Homework9.java
@@ -4,5 +4,25 @@ public class Homework9 {
     public static void main(String[] args){
         int[] arr = {10, 12, 48, 90};
         int insertNum = 23;
+        int index = -1;
+        for (int i = 0; i < arr.length; i++){
+            if (insertNum < arr[i]){
+                index = i;
+                break;
+            }
+        }
+        int[] arrNew = new int[arr.length + 1];
+        for (int i = 0, j = 0; i < arrNew.length; i++){
+            if (i != index){
+                arrNew[i] = arr[j];
+                j++;
+            } else {
+                arrNew[i] = insertNum;
+            }
+        }
+        arr = arrNew;
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
